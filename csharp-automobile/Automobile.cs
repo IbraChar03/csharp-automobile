b57a0ba5d6e482200e03b806b5a828b9ab4270f7
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-08 12:26:43 +0200

    Add project files.

---
+++ csharp-automobile/Automobile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_automobile
+{
+    internal class Automobile
+    {
+       
+        public string marca { get; set;  }
+        public string modello { get; set; }
+        public int cilindrata { get; set; }
+        public int numPorte { get; set; }
+        public int marce { get; set; }
+        public string colore { get; set; }
+        public string carburante { get; set; }
+       
+        public Automobile(string marca, string modello,int  cilindrata, int numPorte, int marce, string colore, string carburante)
+        {
+            this.marca = marca;
+            this.modello = modello;
+            this.cilindrata = cilindrata;
+            this.numPorte = numPorte;
+            this.marce = marce;
+            this.colore = colore;
+            this.carburante = carburante;
+        }
+        public void StampaMacchina()
+        {
+            Console.WriteLine($"Marca : {marca}");
+            Console.WriteLine($"Modello : {modello}");
+            Console.WriteLine($"Cilindrata : {cilindrata}");
+            Console.WriteLine($"Numero porte : {numPorte}");
+            Console.WriteLine($"Numero marce : {marce}");
+            Console.WriteLine($"Colore : {colore}");
+            Console.WriteLine($"Carburante : {carburante}");
+        
+        }
+        public string NomeCompleto()
+        {
+            return marca + " " + modello;
+        }
+    }
+}
---
+++ csharp-automobile/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace csharp_automobile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Crea la tua macchina");
+            Console.Write("Inserisci la marca : ");
+            string marca = Console.ReadLine();
+            Console.Write("Inserisci il modello : ");
+            string modello = Console.ReadLine();
+            Console.Write("Inserisci la cilindrata : ");
+            int cilindrata;
+            while (!int.TryParse(Console.ReadLine(), out cilindrata))
+                Console.WriteLine("Inserisci un numero!");
+            Console.Write("Inserisci il numero di porte : ");
+            int porte;
+            while (!int.TryParse(Console.ReadLine(), out  porte))
+                Console.WriteLine("Inserisci un numero!");
+           
+            Console.Write("Inserisci il numero di marce : ");
+            int marce;
+            while (!int.TryParse(Console.ReadLine(), out marce))
+                 Console.WriteLine("Inserisci un numero!");
+
+            Console.WriteLine("Inserisci il colore : ");
+            string colore = Console.ReadLine();
+            Console.WriteLine("inserisci il tipo di carburante : ");
+            string carburante = Console.ReadLine();
+
+
+            Automobile auto1 = new Automobile(marca, modello, cilindrata, porte, marce, colore, carburante);
+            Console.WriteLine($"Il nome completo dell`auto è {auto1.NomeCompleto()}");
+            Console.WriteLine("Riepilogo della tua macchina...");
+            auto1.StampaMacchina();
+            
+           
+        }
+    }
+}
